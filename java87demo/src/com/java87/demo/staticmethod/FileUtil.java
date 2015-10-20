53b690ab4ba1ccb9acf001376b88ef99efacb7f0
Alex <alexemail7@126.com>
2015-10-20 16:34:00 +0800

    lambda

---
+++ java87demo/src/com/java87/demo/staticmethod/FileUtil.java
@@ -0,0 +1,12 @@
+package com.java87.demo.staticmethod;
+
+/**
+ * 在Java 8中，可以为接口添加静态方法
+ * Created by WY on 2015/10/20.
+ */
+public interface FileUtil {
+
+    public static void test(){
+        System.out.println("test!");
+    }
+}
---
+++ java87demo/src/com/java87/demo/staticmethod/Test.java
@@ -0,0 +1,12 @@
+package com.java87.demo.staticmethod;
+
+/**
+ * Created by WY on 2015/10/20.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        FileUtil.test();
+    }
+
+}
