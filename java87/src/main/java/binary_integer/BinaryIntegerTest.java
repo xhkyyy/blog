39d901ad302e830f9ddc63e0d0f160655f047da4
tnndwc <tnndwc@gmail.com>
2018-08-07 14:45:17 +0800

    binary to integer

---
+++ java87/src/main/java/binary_integer/BinaryIntegerTest.java
@@ -0,0 +1,16 @@
+package binary_integer;
+
+import org.junit.Test;
+
+public class BinaryIntegerTest {
+
+    @Test
+    public void mainx() {
+        System.out.println("\n\n-----------------");
+        // 二进制转换为 Integer
+        System.out.println(Integer.parseInt("111111111111", 2));
+        System.out.println("-----------------\n\n");
+
+    }
+
+}
