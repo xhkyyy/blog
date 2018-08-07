284c04b7074325b72f9db4dd001646a5cf0a04c4
tnndwc <tnndwc@gmail.com>
2018-08-07 13:59:52 +0800

    binary

---
+++ java87/src/main/java/binary_integer/Test.java
@@ -0,0 +1,12 @@
+package binary_integer;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // 二进制转换为 Integer
+        System.out.println(Integer.parseInt("111111111111", 2));
+
+    }
+
+}
