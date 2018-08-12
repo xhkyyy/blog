f1d94c4aee43a3285692b9ead3da5374f05f0605
tnndwc <tnndwc@gmail.com>
2018-08-12 22:46:48 +0800

    netty

--- java87/src/main/java/netty_descard/DiscardServerHandler.java
+++ java87/src/main/java/netty_descard/DiscardServerHandler.java
@@ -1,4 +1,21 @@
 package netty_descard;
 
-public class DiscardServerHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
