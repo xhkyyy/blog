5f2d50d939ae38c85b8c58f22bf4fcb1f5192f11
tnndwc <tnndwc@gmail.com>
2018-08-13 12:07:08 +0800

    close channel(netty)

--- java87/src/main/java/netty_descard/DiscardServerHandler.java
+++ java87/src/main/java/netty_descard/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package netty_descard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 /**
  * Handles a server-side channel.
@@ -10,12 +11,30 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ((ByteBuf) msg).release();
+        //channelReadProcessDiscard(((ByteBuf) msg));
+        channelReadProcessLookingInto(ctx, ((ByteBuf) msg));
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
+    }
+
+    private void channelReadProcessDiscard(ByteBuf msg) {
+        msg.release();
+    }
+
+    private void channelReadProcessLookingInto(ChannelHandlerContext ctx, ByteBuf msg) {
+        try {
+            System.out.println(msg.toString(CharsetUtil.UTF_8));
+
+
+            // the two methods below requests to close the channel.
+            // 1. ctx.close();
+            // 2. ctx.channel().close();
+        } finally {
+            msg.release();
+        }
     }
 }
