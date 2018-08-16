f377d709dd91742693c6607ecda1552a09c35b44
tnndwc <tnndwc@gmail.com>
2018-08-16 15:42:03 +0800

    Concatenate Test

---
+++ java87/src/main/java/jmh_string/ConcatenateTest.java
@@ -0,0 +1,68 @@
+package jmh_string;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConcatenateTest {
+
+    private final static int SIZE = 100;
+
+    private final static  char[] array = RandomStringUtils.randomAlphanumeric(SIZE).toCharArray();
+
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measure1() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 10000; i++) {
+
+            sb.append(array[i % SIZE]);
+
+        }
+
+        System.out.println("measure1 size: " + sb.length());
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measure2() {
+
+        String sb = "";
+
+        for (int i = 0; i < 10000; i++) {
+
+            sb = sb + array[i % SIZE];
+
+        }
+
+        System.out.println("measure2 size: " + sb.length());
+    }
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+
+                .include(ConcatenateTest.class.getSimpleName())
+
+                .forks(1)
+
+                .build();
+
+
+        new Runner(opt).run();
+
+    }
+}
--- java87/src/main/java/jmh_string/README.md
+++ java87/src/main/java/jmh_string/README.md
@@ -4,3 +4,8 @@ StringTest.measure1  avgt    5  0.476 ± 0.079   s/op
 StringTest.measure2  avgt    5  0.279 ± 0.119   s/op
 StringTest.measure3  avgt    5  0.103 ± 0.001   s/op
 
+
+## ConcatenateTest.java
+Benchmark                 Mode  Cnt         Score         Error  Units
+ConcatenateTest.measure1  avgt    5    132021.139 ±   23843.560  ns/op
+ConcatenateTest.measure2  avgt    5  18009578.623 ± 1702636.267  ns/op
