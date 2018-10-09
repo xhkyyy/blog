3c395f05d6dde11f94c2650cb5ad9f9f1605f659
xhkyyy <tnndwc@gmail.com>
2018-10-09 11:01:07 +0800

    jmh

---
+++ java87/src/main/java/demo/other/Demo2.java
@@ -0,0 +1,16 @@
+package demo.other;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+        Map<String, String> m = new HashMap<>(1);
+
+        m.put("a", "b");
+
+    }
+}
