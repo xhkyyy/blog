e7fa80946e5a7f6bb59c166cd5187f2a248d7070
Len <tnndwc@gmail.com>
2020-07-19 22:21:47 +0800

    markdown

--- markdown/IndexMarkdown/build.sh
+++ markdown/IndexMarkdown/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o markdown_linux_amd64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o markdown_darwin_amd64 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o markdown_windows_amd64.exe
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o link-markdown-linux-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o link-markdown-darwin-amd64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o link-markdown-windows-amd64.exe
 
