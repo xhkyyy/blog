066d8a7335479f16f6e250b4fd34c1ebe0ab9727
xhkyyy <tnndwc@gmail.com>
2022-02-08 09:41:22 +0800

    update link-markdown tool

--- markdown/IndexMarkdown/build.sh
+++ markdown/IndexMarkdown/build.sh
@@ -2,5 +2,6 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o link-markdown-linux-amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o link-markdown-darwin-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o link-markdown-darwin-arm64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o link-markdown-windows-amd64.exe
 
